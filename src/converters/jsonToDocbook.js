3a4a9fbf9be5228fb36d7c088fd92ac5d9de2fbe
christianbodart <christian.bodart@gmail.com>
2025-11-27 14:29:20 +0000

    2nd refactor with factories

--- src/converters/jsonToDocbook.js
+++ src/converters/jsonToDocbook.js
@@ -42,88 +42,85 @@ const blockBuilders = {
   }
 };
 
-// Helper: convert markdown inline tokens to DocBook nodes
+// Factory for creating paired element handlers (open/close tokens)
+function createPairedHandler(elementName, attrs = {}, closingType) {
+  return (parent, token, children, iterator) => {
+    const elem = parent.ele(elementName, attrs);
+    iterator.next(); // skip open token
+    while (iterator.current() && iterator.current().type !== closingType) {
+      processToken(elem, iterator.current(), children, iterator);
+      iterator.next();
+    }
+  };
+}
+
+// Simple token handlers
+const tokenHandlers = {
+  text: (parent, token) => parent.txt(token.content),
+  
+  softbreak: (parent) => parent.ele('linebreak'),
+  hardbreak: (parent) => parent.ele('linebreak'),
+  
+  code_inline: (parent, token) => parent.ele('literal').txt(token.content),
+  
+  footnote_ref: (parent, token) => 
+    parent.ele('footnoteref', { linkend: token.meta?.id || '' }),
+  
+  em_open: createPairedHandler('emphasis', { role: 'bold' }, 'em_close'),
+  strong_open: createPairedHandler('emphasis', { role: 'bold' }, 'strong_close'),
+  mark_open: createPairedHandler('phrase', { role: 'mark' }, 'mark_close'),
+  ins_open: createPairedHandler('phrase', { role: 'ins' }, 'ins_close'),
+  
+  link_open: (parent, token, children, iterator) => {
+    const href = token.attrs?.find(([name]) => name === 'href');
+    if (href && href[1].startsWith('#fn')) {
+      parent.ele('footnoteref', { linkend: href[1].slice(1) });
+    } else if (href && href[1].startsWith('#cite')) {
+      parent.ele('citation', { 'xlink:href': href[1] });
+    }
+    // Skip to link_close
+    iterator.next();
+    while (iterator.current() && iterator.current().type !== 'link_close') {
+      iterator.next();
+    }
+  },
+};
+
+// Iterator wrapper for token stream
+class TokenIterator {
+  constructor(children) {
+    this.children = children;
+    this.index = 0;
+  }
+  
+  current() {
+    return this.children[this.index];
+  }
+  
+  next() {
+    this.index++;
+  }
+}
+
+// Process a single token
+function processToken(parent, token, children, iterator) {
+  const handler = tokenHandlers[token.type];
+  if (handler) {
+    handler(parent, token, children, iterator);
+  } else if (token.content) {
+    parent.txt(token.content);
+  }
+}
+
+// Convert markdown inline tokens to DocBook nodes
 function markdownToDocbookNodes(markdownText, parent) {
   const tokens = md.parseInline(markdownText, {});
-  let i = 0;
   const children = tokens[0].children;
-
-  while (i < children.length) {
-    const token = children[i];
-    switch (token.type) {
-      case 'text':
-        parent.txt(token.content);
-        break;
-
-      case 'softbreak':
-      case 'hardbreak':
-        parent.ele('linebreak');
-        break;
-
-      case 'em_open': {
-        const em = parent.ele('emphasis', { role: 'bold' });
-        i++;
-        while (i < children.length && children[i].type !== 'em_close') {
-          markdownToDocbookNodes(children[i].content || children[i].markup, em);
-          i++;
-        }
-        break;
-      }
-
-      case 'strong_open': {
-        const strong = parent.ele('emphasis', { role: 'bold' });
-        i++;
-        while (i < children.length && children[i].type !== 'strong_close') {
-          markdownToDocbookNodes(children[i].content || children[i].markup, strong);
-          i++;
-        }
-        break;
-      }
-
-      case 'code_inline':
-        parent.ele('literal').txt(token.content);
-        break;
-
-      case 'link_open': {
-        const href = token.attrs?.find(([name]) => name === 'href');
-        if (href && href[1].startsWith('#fn')) {
-          parent.ele('footnoteref', { linkend: href[1].slice(1) });
-        } else if (href && href[1].startsWith('#cite')) {
-          parent.ele('citation', { 'xlink:href': href[1] });
-        }
-        while (i < children.length && children[i].type !== 'link_close') i++;
-        break;
-      }
-
-      case 'footnote_ref':
-        parent.ele('footnoteref', { linkend: token.meta?.id || '' });
-        break;
-
-      case 'mark_open': {
-        const mark = parent.ele('phrase', { role: 'mark' });
-        i++;
-        while (i < children.length && children[i].type !== 'mark_close') {
-          markdownToDocbookNodes(children[i].content || children[i].markup, mark);
-          i++;
-        }
-        break;
-      }
-
-      case 'ins_open': {
-        const ins = parent.ele('phrase', { role: 'ins' });
-        i++;
-        while (i < children.length && children[i].type !== 'ins_close') {
-          markdownToDocbookNodes(children[i].content || children[i].markup, ins);
-          i++;
-        }
-        break;
-      }
-
-      default:
-        if (token.content) parent.txt(token.content);
-        break;
-    }
-    i++;
+  const iterator = new TokenIterator(children);
+  
+  while (iterator.current()) {
+    processToken(parent, iterator.current(), children, iterator);
+    iterator.next();
   }
 }
 
